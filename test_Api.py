510a01b56075df543d7fbe0e996f0116efc8fa0d
liuzhihui <liuzhihui0911@gmail.com>
2021-03-16 18:31:09 +0800

    1. 使用@pytest.fixtur()，控制调用获取token的次数 2. 使用@pytest.mark.parametrize("",[])，实现参数化 3. 列表生成式实现多数据同时新建删除

--- test_Api.py
+++ test_Api.py
@@ -3,45 +3,64 @@ import requests
 import pytest
 
 
+@pytest.fixture(scope="session")
+def get_token():
+    params = {
+        "corpid": "ww63762cffdeedb367",
+        "corpsecret": "GmsE-Gy51M3hEI46OSaTR2JpZ7aPpLymBYQjl4SlAxQ"
+    }
+    res_token = requests.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken", params=params)
+    return res_token.json()["access_token"]
+
+
 class TestAPI(object):
-    @pytest.mark.run(order=1)
-    def test_get_token(self):
-        params = {
-            "corpid": "ww63762cffdeedb367",
-            "corpsecret": "GmsE-Gy51M3hEI46OSaTR2JpZ7aPpLymBYQjl4SlAxQ"
-        }
-        res_token = requests.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken", params=params)
-        return res_token.json()["access_token"]
 
-    def test_get_department_list(self):
-        res_get = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token={self.test_get_token()}&id={''}")
-        print(res_get.json())
-        assert res_get.json()["errcode"] == 0
+    def test_get_department_list(self, get_token, id=None):
+        res_get = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token={get_token}&id={id}")
 
-    def test_add_department(self):
+        return res_get.json()
+
+    def test_add_department(self, get_token, name, parentid, id):
         data = {
-            "name": "广州研发中心",
-            "parentid": 1,
-            "id": 2
+            "name": name,
+            "parentid": parentid,
+            "id": id
         }
+        res_add = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/department/create?access_token={get_token}", data=json.dumps(data))
+        return res_add.json()
 
-        res_add = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/department/create?access_token={self.test_get_token()}", data=json.dumps(data))
-        print(res_add.json())
-        print(res_add.content)
-        assert res_add.json()["errcode"] == 0
-
-    def test_update_department(self):
+    def test_update_department(self, get_token, cur_id, name, name_en=None, parentid=None, order=None):
         data = {
-            "id": 2,
-            "name": "广东研发中心"
+            "id": cur_id,
+            "name": name,
+            "name_en": name_en,
+            "parentid": parentid,
+            "order": order
         }
+        res_update = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/department/update?access_token={get_token}", json=data)
+        return res_update.json()
 
-        res_update = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/department/update?access_token={self.test_get_token()}", json=data)
-        print(res_update.json())
-        assert res_update.json()["errcode"] == 0
+    def test_delete_department(self, get_token, id):
+        res_del = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/department/delete?access_token={get_token}&id={id}")
+        return res_del.json()
 
-    def test_delete_department(self):
+    info = [("山东研发中心" + str(x), "1", str(x)) for x in range(10, 20)]
 
-        res_del = requests.get(f"https://qyapi.weixin.qq.com/cgi-bin/department/delete?access_token={self.test_get_token()}&id=2")
-        print(res_del.json())
-        assert res_del.json()["errcode"] == 0
+    @pytest.mark.parametrize("name, parentid, id", info)
+    def test_all(self, get_token, name, parentid, id):
+        # 若创建异常
+        try:
+            assert "created" == self.test_add_department(get_token, name, parentid, id)["errmsg"]
+        except AssertionError as e:
+            if "department existed" in e.__str__():
+                department_info = self.test_get_department_list(get_token)
+                num = len(department_info["department"])
+                for i in range(num):
+                    if department_info["department"][i]["name"] == name:
+                        id = department_info["department"][i]["id"]
+                        assert "deleted" == self.test_delete_department(get_token, id)['errmsg']
+                assert "created" == self.test_add_department(get_token, name, parentid, id)["errmsg"]
+        finally:
+            assert name in [info["name"] for info in self.test_get_department_list(get_token)["department"]]
+            assert "updated" == self.test_update_department(get_token, id, "德州研发中心")["errmsg"]
+            assert "deleted" == self.test_delete_department(get_token, id)['errmsg']
